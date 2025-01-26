c426dec2e14ecabdd45cf7659878c1a7ec6ef2b5
matt-dong-123 <148605800+matt-dong-123@users.noreply.github.com>
2025-01-26 22:18:55 +0800

    added install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+echo "Pay the bill first. For the payment, we need some sensitive information."
+read -p "Enter your social security number/id number: " ssn
+echo "Your social security number/id number is $ssn. We have stored it in a private (cough cough) and end-to-end encrypted (coughing intensifies) database."
+echo "Pay the bill though. We are in debt."
